c5685846e5d08ec2279edf4d3248b8642c7735d6
ZekaEu <zeka.surf@hotmail.com>
2022-08-03 04:55:02 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    first_list = list(first_string.lower())
+    second_list = list(second_string.lower())
+    for letter in first_list:
+        try:
+            second_list.remove(letter)
+        except ValueError:
+            return False
+    return True
