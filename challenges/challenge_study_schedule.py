030a2d87cae71134b9794174cf66ecaa8693bd26
ZekaEu <zeka.surf@hotmail.com>
2022-08-03 04:10:23 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    time_counter = 0
+    for login_time, logout_time in permanence_period:
+        try:
+            if login_time <= target_time <= logout_time:
+                time_counter += 1
+        except TypeError:
+            return None
+    return time_counter
